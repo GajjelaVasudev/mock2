1625ea812c998cc5b2fd87eaa0b2bfe6200a51a9
GajjelaVasudev <vgajjella@gmail.com>
2026-09-16 11:24:30 +0530

    Auth middleware added

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
-const authenticate = require('../middleware/auth.middleware');
+const authenticate = require('../middlewares/auth.middleware');
 
 router.post('/register', authController.registerUser);
 router.post('/login', authController.LoginUser);
