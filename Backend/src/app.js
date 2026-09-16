cc50abe88d6a8fcb2e1922b62bc698abd4ea7bbf
GajjelaVasudev <vgajjella@gmail.com>
2026-09-16 12:14:45 +0530

    Added Self-assesment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,7 @@ const app = express();
 const authRoutes = require('./routes/auth.routes');
 const studentRoutes = require('./routes/student.routes');
 const contentRoutes = require('./routes/content.routes');
+const assessmentRoutes = require('./routes/assesment.routes');
 
 
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/learners', studentRoutes);
 app.use('/api/content', contentRoutes);
+app.use('/api/self-assessments', assessmentRoutes);
 
 
 module.exports = app;
