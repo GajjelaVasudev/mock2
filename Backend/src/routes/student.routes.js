f7606cf193acf1004ec9ac4d28d44491c2b57579
GajjelaVasudev <vgajjella@gmail.com>
2026-09-16 11:50:02 +0530

    Update feature for student's

--- Backend/src/routes/student.routes.js
+++ Backend/src/routes/student.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/student.controller');
 const authenticate = require('../middlewares/auth.middleware');
+const requireRole = require('../middlewares/role.middleware');
 
-router.get('/me', authenticate, studentController.getMyProfile);
+router.get('/me', authenticate, requireRole('student', 'alumni'), studentController.getMyProfile);
+router.put('/me', authenticate, requireRole('student', 'alumni'), studentController.updateMyProfile);
 
 module.exports = router;
